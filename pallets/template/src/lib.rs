c8ace728d4c2451c8be4b6f8b3a6ef9b37690bf1
esbrown <esbrown@stanford.edu>
2022-06-01 21:08:38 +0100

    add format on save

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -14,32 +14,42 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
+	use codec::Codec;
 	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+	use frame_system::{
+		offchain::{SendTransactionTypes, SubmitTransaction},
+		pallet_prelude::*,
+	};
+	use scale_info::TypeInfo;
 	use sp_runtime::{
 		offchain::{
-			storage::{StorageValueRef},
+			storage::StorageValueRef,
 			storage_lock::{StorageLock, Time},
 		},
-		traits::{
-			AtLeast32BitUnsigned, Saturating, Zero
-		}
+		traits::{AtLeast32BitUnsigned, Saturating, Zero},
 	};
-	use frame_system::offchain::{SubmitTransaction, SendTransactionTypes};
-	use codec::{Codec};
-	use sp_std::{fmt::Debug};
-	use scale_info::TypeInfo;
+	use sp_std::fmt::Debug;
 
 	const DB_KEY_SUM: &[u8] = b"donations/txn-fee-sum";
 	const DB_LOCK: &[u8] = b"donations/txn-sum-lock";
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
-	pub trait Config: frame_system::Config + pallet_balances::Config + SendTransactionTypes<Call<Self>>{
+	pub trait Config:
+		frame_system::Config + pallet_balances::Config + SendTransactionTypes<Call<Self>>
+	{
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-		type BalancesEvent: From<<Self as frame_system::Config>::Event> + TryInto<pallet_balances::Event<Self>>;
-		type Balance: AtLeast32BitUnsigned + Saturating + Codec + TypeInfo + Default + Debug + Copy + From<<Self as pallet_balances::Config>::Balance>;
-		
+		type BalancesEvent: From<<Self as frame_system::Config>::Event>
+			+ TryInto<pallet_balances::Event<Self>>;
+		type Balance: AtLeast32BitUnsigned
+			+ Saturating
+			+ Codec
+			+ TypeInfo
+			+ Default
+			+ Debug
+			+ Copy
+			+ From<<Self as pallet_balances::Config>::Balance>;
+
 		// constants
 
 		// Transaction priority for the unsigned transactions
@@ -54,10 +64,13 @@ pub mod pallet {
 
 	// The next block where an unsigned transaction will be considered valid
 	#[pallet::type_value]
-	pub(super) fn DefaultNextUnsigned<T: Config>() -> T::BlockNumber { T::BlockNumber::from(0u32) }
+	pub(super) fn DefaultNextUnsigned<T: Config>() -> T::BlockNumber {
+		T::BlockNumber::from(0u32)
+	}
 	#[pallet::storage]
 	#[pallet::getter(fn next_unsigned_at)]
-	pub(super) type NextUnsignedAt<T: Config> = StorageValue<_, T::BlockNumber, ValueQuery, DefaultNextUnsigned<T>>;
+	pub(super) type NextUnsignedAt<T: Config> =
+		StorageValue<_, T::BlockNumber, ValueQuery, DefaultNextUnsigned<T>>;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -79,12 +92,12 @@ pub mod pallet {
 		InvalidOffchainStorageRead,
 	}
 
-    #[pallet::hooks]
-    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		// Each block, initiate an offchain worker to summarize the txn fees for that block,
 		// and append that amount to a counter in local storage, which we will empty
 		// when it is time to send the txn fees to Sequester.
-		fn offchain_worker(block_number: T::BlockNumber){
+		fn offchain_worker(block_number: T::BlockNumber) {
 			let block_fee_sum = Self::calculate_fees_for_block();
 
 			log::info!("total fees for block!!: {:?}", block_fee_sum);
@@ -113,12 +126,12 @@ pub mod pallet {
 				// Reject outdated txns
 				let next_unsigned_at = Self::next_unsigned_at();
 				if &next_unsigned_at > block_num {
-					return InvalidTransaction::Stale.into();
+					return InvalidTransaction::Stale.into()
 				}
 				// Reject txns from the future
 				let current_block = <frame_system::Pallet<T>>::block_number();
 				if &current_block < block_num {
-					return InvalidTransaction::Future.into();
+					return InvalidTransaction::Future.into()
 				}
 
 				log::info!("valid unsigned transaction -- sending {:?} to sequester", amount);
@@ -150,12 +163,12 @@ pub mod pallet {
 			Ok(None.into())
 		}
 	}
-	
+
 	impl<T: Config> Pallet<T> {
 		fn calculate_fees_for_block() -> <T as Config>::Balance {
 			let events = <frame_system::Pallet<T>>::read_events_no_consensus();
 
-			let mut curr_block_fee_sum = Zero::zero(); 
+			let mut curr_block_fee_sum = Zero::zero();
 			let mut withdrawee: Option<T::AccountId> = None;
 
 			let filtered_events = events.into_iter().filter_map(|event_record| {
@@ -164,32 +177,39 @@ pub mod pallet {
 			});
 
 			for event in filtered_events {
-				Self::match_event(event, & mut withdrawee, & mut curr_block_fee_sum);
+				Self::match_event(event, &mut withdrawee, &mut curr_block_fee_sum);
 			}
 
 			curr_block_fee_sum
 		}
 
-		fn match_event(event: pallet_balances::Event<T>, withdrawee: & mut Option<T::AccountId>, curr_block_fee_sum: & mut<T as Config>::Balance) {
+		fn match_event(
+			event: pallet_balances::Event<T>,
+			withdrawee: &mut Option<T::AccountId>,
+			curr_block_fee_sum: &mut <T as Config>::Balance,
+		) {
 			match event {
-				<pallet_balances::Event<T>>::Withdraw{who, amount} => {
+				<pallet_balances::Event<T>>::Withdraw { who, amount } => {
 					*withdrawee = who.into();
 					log::info!("withdraw event!!: {:?}", amount);
-					*curr_block_fee_sum = (*curr_block_fee_sum).saturating_add(<T as Config>::Balance::from(amount));
+					*curr_block_fee_sum =
+						(*curr_block_fee_sum).saturating_add(<T as Config>::Balance::from(amount));
 				},
-				<pallet_balances::Event<T>>::Deposit{who, amount} => {
-					// If amount is deposited back into the account that paid for the transaction fees
-					// during the same transaction, then deduct it from the txn fee counter as a refund
+				<pallet_balances::Event<T>>::Deposit { who, amount } => {
+					// If amount is deposited back into the account that paid for the transaction
+					// fees during the same transaction, then deduct it from the txn fee counter as
+					// a refund
 					if Some(who) == *withdrawee {
 						log::info!("deposit refunded!!: {:?}", amount);
-						*curr_block_fee_sum = (*curr_block_fee_sum).saturating_sub(<T as Config>::Balance::from(amount));
+						*curr_block_fee_sum = (*curr_block_fee_sum)
+							.saturating_sub(<T as Config>::Balance::from(amount));
 					}
 				},
-				_ => {}
+				_ => {},
 			}
 		}
 
-		fn update_storage(block_fee_sum: <T as Config>::Balance){
+		fn update_storage(block_fee_sum: <T as Config>::Balance) {
 			// Use get/set instead of mutation to guarantee that we don't
 			// hit any MutateStorageError::ConcurrentModification errors
 			let mut lock = StorageLock::<Time>::new(&DB_LOCK);
@@ -199,20 +219,27 @@ pub mod pallet {
 				match val.get::<<T as Config>::Balance>() {
 					// initialize value
 					Ok(None) => {
-						log::info!("initializing storage val and setting it to: {:?}", block_fee_sum);
+						log::info!(
+							"initializing storage val and setting it to: {:?}",
+							block_fee_sum
+						);
 						val.set(&block_fee_sum);
 					},
 					// update value
 					Ok(Some(fetched_txn_fee_sum)) => {
-						log::info!("retrieved storage val: {:?} and adding : {:?}", fetched_txn_fee_sum, block_fee_sum);
+						log::info!(
+							"retrieved storage val: {:?} and adding : {:?}",
+							fetched_txn_fee_sum,
+							block_fee_sum
+						);
 						val.set(&fetched_txn_fee_sum.saturating_add(block_fee_sum));
 					},
-					_ => {}
+					_ => {},
 				};
 			}
 		}
 
-		fn send_fees_to_sequester(block_num: T::BlockNumber){
+		fn send_fees_to_sequester(block_num: T::BlockNumber) {
 			// get lock so that another ocw doesn't modify the value mid-send
 			let mut lock = StorageLock::<Time>::new(&DB_LOCK);
 			{
@@ -221,25 +248,25 @@ pub mod pallet {
 				let fees_to_send = val.get::<<T as Config>::Balance>();
 				match fees_to_send {
 					Ok(Some(fetched_fees)) => {
-						let call = Call::<T>::submit_unsigned{amount: fetched_fees, block_num};
-						let txn_res = SubmitTransaction::<T, Call<T>>::submit_unsigned_transaction(call.into()).map_err(|_| {
-						});
+						let call = Call::<T>::submit_unsigned { amount: fetched_fees, block_num };
+						let txn_res = SubmitTransaction::<T, Call<T>>::submit_unsigned_transaction(
+							call.into(),
+						)
+						.map_err(|_| {});
 						match txn_res {
 							Ok(_) => {
 								log::info!("resetting storage value");
-								let zero_bal:<T as Config>::Balance = Zero::zero(); 
+								let zero_bal: <T as Config>::Balance = Zero::zero();
 								val.set(&zero_bal);
 							},
 							Err(_) => {
 								log::error!("Failed in offchain_unsigned_tx");
-							}
+							},
 						}
 					},
 					_ => {},
 				};
 			}
-
 		}
-
 	}
 }
